08682f4b56158e4db2aa40097dbc6b0df62151b9
detectiveseo <detectiveseo1@gmai.com>
2023-10-14 12:48:38 +0600

    created files

---
+++ src/components/counter/index.tsx
@@ -0,0 +1,9 @@
+const CounterComponent = () => {
+    return (
+        <div>
+            <h1>This is the counter component</h1>
+        </div>
+    );
+};
+
+export default CounterComponent;<h1>This is the counter component</h1>
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1 className="text-9xl font-bold">This Is The Home page</h1>
+        </div>
+    );
+};
+
+export default HomePage;
